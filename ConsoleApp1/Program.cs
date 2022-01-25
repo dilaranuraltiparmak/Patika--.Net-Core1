38f4ab30bb45a3c4a16deac503d51dfccf128a51
dilar <dilar@DESKTOP-2L7N6Q6>
2022-01-25 13:47:34 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Lütfen adınızı giriniz");
+string name = Console.ReadLine();
+Console.WriteLine("Lütfen soyadınızı giriniz");
+string surname = Console.ReadLine();
+Console.WriteLine("Merhaba " +name + " " +surname );
